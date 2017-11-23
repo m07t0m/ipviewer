3e5d7fd4da0991090ade435bb83daf046313cf9a
m07t0m <tmharry@gmail.com>
2017-11-23 11:03:08 +0100

    reload ip working

--- ipviewer_frontend/src/js/pages/Settings.js
+++ ipviewer_frontend/src/js/pages/Settings.js
@@ -1,42 +1,71 @@
 import React from "react";
 
+import * as ipActions from "../actions/ipActions";
+import IPStore from "../stores/IPStore";
+
+
 export default class Settings extends React.Component {
+
+	constructor() {
+		super();
+		this.reloadIP();
+		this.getTodos = this.getTodos.bind(this);
+		this.updateInputValue = this.updateInputValue.bind(this);
+		this.state = {
+		  ips: IPStore.getAllIP(),
+		  inputfield:''
+		};
+  	}
+  updateInputValue(evt){
+    //console.log("input field updated with "+evt.target.value);
+    this.state={inputfield: evt.target.value};   
+
+  }
+	getTodos() {
+	    this.setState({      
+	      ips: IPStore.getAllIP()
+	    });
+  }
+  
+  reloadIP() {
+    ipActions.reloadIP();
+  }
+
+  componentWillMount() {
+    IPStore.on("change", this.getTodos);
+  }
+
+  componentWillUnmount() {
+    IPStore.removeListener("change", this.getTodos);
+  }
   render() {
+  	 const { ips } = this.state;
+  	 
+  	 const IPScomponents = Object.keys(ips).map(function(key){
+      if(ips[key].hasOwnProperty("hostname")){
+        return <tr><td><span>{key}</span></td> <td><span>{ips[key].vendor}</span></td> <td><span>{ips[key].mac}</span></td></tr>
+      }
+ 	})
+      
     return (
       <div>
-        <h1>Settings?</h1>
-
+        <h1>Received IP details  <button onClick={this.reloadIP.bind(this)} type="submit" class="btn btn-primary btn-lg">Reload</button></h1>
+        
         <div class="container">
-		  <h2>Bordered Table</h2>
-		  <p>The .table-bordered class adds borders to a table:</p>            
 		  <table class="table table-bordered">
 		    <thead>
 		      <tr>
 		        <th>IP</th>
-		        <th>endor</th>
+		        <th>Vendor</th>
 		        <th>Email</th>
 		      </tr>
 		    </thead>
-		    <tbody>
-		      <tr>
-		        <td>John</td>
-		        <td>Doe</td>
-		        <td>john@example.com</td>
-		      </tr>
-		      <tr>
-		        <td>Mary</td>
-		        <td>Moe</td>
-		        <td>mary@example.com</td>
-		      </tr>
-		      <tr>
-		        <td>July</td>
-		        <td>Dooley</td>
-		        <td>july@example.com</td>
-		      </tr>
-		    </tbody>
+		    <tbody>{IPScomponents}</tbody>
 		  </table>
 		</div>
+
       </div>
     );
   }
-}
+ }
+
---
+++ ipviewer_frontend/src/js/pages/Todos.js
@@ -0,0 +1,103 @@
+import React from "react";
+
+import Todo from "../components/Todo";
+import * as TodoActions from "../actions/TodoActions";
+import TodoStore from "../stores/TodoStore";
+
+
+export default class Featured extends React.Component {
+  constructor() {
+    super();
+    this.reloadIP.bind(this);
+    this.getTodos = this.getTodos.bind(this);
+    this.updateInputValue = this.updateInputValue.bind(this);
+    this.state = {
+      ips: IPStore.getAllIP(),
+      inputfield:''
+    };
+  }
+
+  componentWillMount() {
+    IPStore.on("change", this.getTodos);
+  }
+
+  componentWillUnmount() {
+    TodoStore.removeListener("change", this.getTodos);
+  }
+
+  getTodos() {
+    this.setState({      
+      ips: TodoStore.getAllIP()
+    });
+  }
+
+  updateInputValue(evt){
+    //console.log("input field updated with "+evt.target.value);
+    this.state={inputfield: evt.target.value};   
+
+  }
+
+  createTodos() {
+    TodoActions.createTodo(this.state.inputfield);
+  }
+
+  createIP() {
+    TodoActions.setIP(this.state.inputfield);
+  }
+
+  reloadTodos() {
+    TodoActions.reloadTodos();
+  }
+
+  reloadIP() {
+    TodoActions.reloadIP();
+  }
+  render() {
+    const { todos } = this.state;
+    const { ips } = this.state;
+    const { inputfield } = this.state;
+    const TodoComponents = todos.map((todo) => {
+        return <Todo key={todo.id} {...todo}/>;
+    });
+    /*
+    const IPScomponents = Object.keys(ips).map(function(key){
+      if(ips[key].hasOwnProperty("hostname")){
+        return <li><span>{key}</span> <span>{ips[key].vendor}</span> <span>{ips[key].mac}</span></li>
+      }
+      
+    })
+    */
+    //const IPScomponents = [];
+    //for (var key in ips){
+    //  IPScomponents.add(<li> {key} </li> ) 
+    //}
+    //const IPScomponents = ips.map((ip) => {
+    //  return <li><span>{ip.ip}</span><span>{ip.mac}</span><span>{ip.vendor}</span> </li>
+    //})
+    /*
+    return (
+      <div>        
+        <button onClick={this.createIP.bind(this)}>CreateIP</button>
+        <button onClick={this.reloadTodos.bind(this)}>Reload!</button>
+        <button onClick={this.reloadIP.bind(this)}>Reload IP!</button>
+        <input type="text" onChange={this.updateInputValue} ></input>
+        <h1>Todos</h1>
+        <ul>{TodoComponents}</ul>
+        <h1>IPs</h1>
+        <ul>{IPScomponents}</ul>
+      </div>
+    );
+    */
+    return (
+      <div class="container">
+        <h1>Set new IP</h1>
+        <form>
+          <div class="form-group">
+            <input class="form-control" placeholder="IP" type="text" onChange={this.updateInputValue} ></input>
+          </div>
+          <button type="submit" onClick={this.createIP.bind(this)} class="btn btn-primary btn-lg">Submit</button>
+        </form>
+      </div>
+      )
+  }
+}
